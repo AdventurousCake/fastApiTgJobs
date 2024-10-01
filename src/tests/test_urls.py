53e79af795ced22e41f44da40a0d2a40935ecf13
Igor <tropitzyn.igor1@gmail.com>
2024-10-01 18:02:38 +0500

    upd

---
+++ src/tests/test_urls.py
@@ -0,0 +1,36 @@
+import pytest
+
+from src.PROJ.api.gen_test_data import init_fake_data
+
+
+# from fastapi.testclient import TestClient as test_client
+
+
+class TestNewpage:
+    def test_newpage(test_client):
+        response = test_client.get('/newpage')
+        assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        # "/jobs",
+        "/jobs/hrs_all",
+        "/jobs/jobs_all",
+        "/docs",
+        # "/newpage",
+        # "/404",
+    ],
+    # indirect=True
+)
+async def test_urls(ac, url):
+    await init_fake_data(limit=10)
+
+    response = await ac.get(url)
+    assert response.status_code == 200
+
+
+# def test_urls(test_client, url):
+#     response = test_client.get(url)
+#     assert response.status_code == 200
