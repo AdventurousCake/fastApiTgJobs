23b9fba808745f28382711237f304cb4d61e3ea3
Igor <tropitzyn.igor1@gmail.com>
2024-10-02 12:51:08 +0500

    upd

--- src/PROJ/core/config.py
+++ src/PROJ/core/config.py
@@ -22,6 +22,7 @@ TEST_DB_PORT = os.getenv("TEST_DB_PORT")
 TEST_DB_NAME = os.getenv("TEST_DB_NAME")
 TEST_DB_USER = os.getenv("TEST_DB_USER")
 TEST_DB_PASS = os.getenv("TEST_DB_PASS")
+TEST_DB_URL = os.getenv("TEST_DB_URL")
 
 JWT_KEY = os.getenv("JWT_KEY")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
@@ -29,7 +30,9 @@ JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 MODE = os.getenv("MODE")
 
 DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-TEST_DB_URL = f"postgresql+asyncpg://{TEST_DB_USER}:{TEST_DB_PASS}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
+if TEST_DB_HOST:
+    TEST_DB_URL = f"postgresql+asyncpg://{TEST_DB_USER}:{TEST_DB_PASS}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
+
 
 # pyro
 TG_SESSION_STRING = os.getenv("TG_SESSION_STRING")
