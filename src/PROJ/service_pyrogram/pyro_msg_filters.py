6ba8942183545dbb5406e9179f678dd0e3a247b3
i <i@github.com>
2025-06-17 20:16:30 +0500

    upd

--- src/PROJ/service_pyrogram/pyro_msg_filters.py
+++ src/PROJ/service_pyrogram/pyro_msg_filters.py
@@ -20,7 +20,7 @@ class VacancyFilter:
     # bool(VACANCY_PATTERN.search(text))
 
     # TUPLES ONLY
-    SENIOR_KEYS = ("#lead", "senior", "#teamlead", "team lead", "#techlead", "#c++")
+    SENIOR_KEYS = ("#lead", "senior", "#teamlead", "team lead", "#techlead", "technical lead", "#c++")
     SENIOR_KEYS_EXCLUDE = ("middle", "junior")
     REMOTE_KEYS = ("#удаленка", "#remote", "#удаленно")
     STARTUP_KEYS = ("стартап", "startup")
